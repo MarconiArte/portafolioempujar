4b60ce07ae3996adb3b44beb4c7e7c39929601c2
Marcos Niz <marconi4305@gmail.com>
2022-11-25 11:37:55 -0300

    Se agrego funcion al segundo boton

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,24 @@ const body = document.querySelector('#body')
 const divUno = document.querySelector('#divUno')
 
 btnUno.addEventListener('click', () =>{
+    body.classList.remove('fondoTres');
     body.classList.add('fondoUno');
     body.classList.toggle('fondoDos');
+    divUno.innerHTML = `
+        <div class="presentacion-contenedor">
+            <div class="presentacion">
+                <h2>Marcos Niz Developer front-end</h2>
+                <p>¡Bienvenido a mi portafolio!<p>
+                <p>Podras encontrar todos mis trabajos, tanto de ilustrador como desarrollador.
+            </div>
+        </div>
+    
+    `
 })
 
 btnDos.addEventListener('click', () =>{
     body.classList.remove('fondoDos', 'fondoUno');
+    body.classList.add('fondoTres');
     divUno.innerHTML = `
         <div class="presentacion-contenedor">
             <div class="presentacion">
@@ -22,4 +34,14 @@ btnDos.addEventListener('click', () =>{
         </div>
     
     `
+})
+
+btnTres.addEventListener('click', () =>{
+    divUno.innerHTML = `
+    <div class="presentacion-contenedor">
+            <div class="presentacion">
+                <h2>Marcos Niz Developer front-end</h2>
+                <p>Botón inútil por el momento</p>
+            </div>
+        </div>`
 })
